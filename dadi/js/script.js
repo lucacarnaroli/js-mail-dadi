5ac500b954ed2c415e59748d23ff1591a21aec8b
luca <luca.carnaroli98@gmail.com>
2020-01-07 17:58:58 +0100

    fine

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -5,3 +5,15 @@ var player2 = Math.floor(Math.random() * 6) + 1;
 // risultati
 console.log(player1);
 console.log(player2);
+
+// vincitore/perdente/pareggio
+
+if (player1 > player2) {
+  console.log('Player1 hai vinto');
+} else if (player1 < player2) {
+  console.log('Player2 hai vinto');
+} else {
+  console.log('Pareggio');
+}
+
+document.getElementById('ok').innertHTML =  player1;
