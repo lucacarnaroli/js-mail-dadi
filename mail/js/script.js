7aa3a61aa6561e138817a5930322bc5f9faa50f7
luca <luca.carnaroli98@gmail.com>
2020-01-07 16:37:50 +0100

    for

--- mail/js/script.js
+++ mail/js/script.js
@@ -5,12 +5,12 @@ console.log(emailUtente);
 var emailUtenti = ['giuseppearimatea10@gmail.com','nicola.ba1989@gmail.com',
 'andrea.bottini22@gmail.com','federico.caiello1994@gmail.com','danilo.campagnolo.93@gmail.com','marcocarone29@gmail.com','luca.carnaroli98@gmail.com','saninoc84@gmail.com','alessandro.corneli11@gmail.com','emanuele.correggia86@gmail.com','alessandrodigiacomantonio94@gmail.com','manueldipilla3@gmail.com','andreademarco91@gmail.com','ghyzounette10@gmail.com','eboulet87@gmail.com','fiore.gianuario@gmail.com','lorenzo.franzone@gmail.com','dnlshiva@gmail.com','simonelega.sl@gmail.com','luisa.logozzo@gmail.com','luglidavide.web@gmail.com','matteo.magliano01@gmail.com','magliozzierasmo98@gmail.com','pacifico.mensa@gmail.com','francesco.pan@gmail.com','palumbosimona118@gmail.com','eliapari93@gmail.com','marcopa84@gmail.com','mattiapedone93@gmail.com','matteop18@gmail.com','riccio.stefano0@gmail.com','tsergata@gmail.com','tarantinosimonew@gmail.com','trottamanuela88@gmail.com']
 
-for (var i = 0; i = nomeUtente.length;) {
-  nomeUtente[i]
-// if (emailUtente == emailUtenti) {
-//   prompt('accesso consentito');
-//   console.log(emailUtente);
-// }
+for (var i = 0; i < emailUtenti.length; i++) {
+  console.log(emailUtenti[i]);
+if (emailUtente == 'emailUtente') {
+  console.log('benvenuto');
+  alert('benvenuto')
+}
 }
 
 
