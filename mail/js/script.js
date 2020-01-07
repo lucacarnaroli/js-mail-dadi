43c7930d90f9ea2984141b689cc875f9b9302ae3
luca <luca.carnaroli98@gmail.com>
2020-01-07 16:59:25 +0100

    true, false

--- mail/js/script.js
+++ mail/js/script.js
@@ -3,14 +3,23 @@ console.log(emailUtente);
 
 // controllare che sia nella lista
 var emailUtenti = ['giuseppearimatea10@gmail.com','nicola.ba1989@gmail.com',
-'andrea.bottini22@gmail.com','federico.caiello1994@gmail.com','danilo.campagnolo.93@gmail.com','marcocarone29@gmail.com','luca.carnaroli98@gmail.com','saninoc84@gmail.com','alessandro.corneli11@gmail.com','emanuele.correggia86@gmail.com','alessandrodigiacomantonio94@gmail.com','manueldipilla3@gmail.com','andreademarco91@gmail.com','ghyzounette10@gmail.com','eboulet87@gmail.com','fiore.gianuario@gmail.com','lorenzo.franzone@gmail.com','dnlshiva@gmail.com','simonelega.sl@gmail.com','luisa.logozzo@gmail.com','luglidavide.web@gmail.com','matteo.magliano01@gmail.com','magliozzierasmo98@gmail.com','pacifico.mensa@gmail.com','francesco.pan@gmail.com','palumbosimona118@gmail.com','eliapari93@gmail.com','marcopa84@gmail.com','mattiapedone93@gmail.com','matteop18@gmail.com','riccio.stefano0@gmail.com','tsergata@gmail.com','tarantinosimonew@gmail.com','trottamanuela88@gmail.com']
+'andrea.bottini22@gmail.com','federico.caiello1994@gmail.com','danilo.campagnolo.93@gmail.com','marcocarone29@gmail.com','luca.carnaroli98@gmail.com','saninoc84@gmail.com','alessandro.corneli11@gmail.com','emanuele.correggia86@gmail.com','alessandrodigiacomantonio94@gmail.com','manueldipilla3@gmail.com','andreademarco91@gmail.com','ghyzounette10@gmail.com','eboulet87@gmail.com','fiore.gianuario@gmail.com','lorenzo.franzone@gmail.com','dnlshiva@gmail.com','simonelega.sl@gmail.com','luisa.logozzo@gmail.com','luglidavide.web@gmail.com','matteo.magliano01@gmail.com','magliozzierasmo98@gmail.com','pacifico.mensa@gmail.com','francesco.pan@gmail.com','palumbosimona118@gmail.com','eliapari93@gmail.com','marcopa84@gmail.com','mattiapedone93@gmail.com','matteop18@gmail.com','riccio.stefano0@gmail.com','tsergata@gmail.com','tarantinosimonew@gmail.com','trottamanuela88@gmail.com'];
+
+var mailPresente = false;
 
 for (var i = 0; i < emailUtenti.length; i++) {
   console.log(emailUtenti[i]);
-if (emailUtente == 'emailUtente') {
+ if (emailUtente == emailUtenti[i]) {
   console.log('benvenuto');
-  alert('benvenuto')
+  mailPresente = true;
+ }
 }
+
+if (mailPresente == true) {
+  alert('benvenuto');
+}
+else {
+  alert('accesso negato');
 }
 
 
